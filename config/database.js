30d8cedaee0453ca983e050b0916aa7b52883926
Zetrey13 <kotaineserge@gmail.com>
2023-05-21 12:29:13 +0000

    fixe database connection

--- config/database.js
+++ config/database.js
@@ -4,7 +4,7 @@ const { DB_CON_STRING } = process.env
 
 module.exports = () => {
     // mogoose.connect("mongodb://localhost/ecommerce")
-    mogoose.connect("mongodb+srv://abidrazaa:Abcd1234@cluster0.lr2rk.mongodb.net/?retryWrites=true&w=majority")
+    mogoose.connect(`${DB_CON_STRING}`)
         .then(() => console.log('DB Connection Successfull'))
         .catch(err => console.log(err.message))
 }
